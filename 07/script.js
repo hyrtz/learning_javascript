3c5cfe0b32a8850b3a60cd8ee0917ffc01c75c64
Mira Desaville <miradeldesaville@gmail.com>
2025-07-31 16:01:53 +0800

    Lesson 9 ongoing

---
+++ 07/script.js
@@ -0,0 +1,38 @@
+
+
+
+
+/* 
+const randomNumber = Math.random(); 
+let result = '';
+let guess = ''; 
+
+function pickComputerMove() {
+  // computer move
+  if (randomNumber < 0.5) {
+    result = 'heads';
+  } else {
+    result = 'tails';
+  }
+}
+
+function userPickedHeads() {
+  if (result === 'heads') {
+    guess = 'You win!'; 
+  } else {
+    guess = 'You lose!'; 
+  }
+
+  alert(`You pick heads. Result is ${result}. ${guess}`);
+}
+
+function userPickedTails() {
+  if (result === 'tails') {
+    guess = 'You win!'; 
+  } else {
+    guess = 'You lose!'; 
+  }
+
+  alert(`You pick tails. Result is ${result}. ${guess}`);
+}
+*/ 
