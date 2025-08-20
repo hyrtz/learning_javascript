c2cfd052b525b48bb51e534c38779907b9129f40
Mira Desaville <miradeldesaville@gmail.com>
2025-08-20 21:51:40 +0800

    Used .reduce() to find the max and min numbers, and shortest and longest word

--- scripts/find_longest_word.js
+++ scripts/find_longest_word.js
@@ -35,3 +35,53 @@ function findShortestWord(array) {
   }
   return shortestWord;
 }
+
+/*
+using .reduce() 
+  - reduce the elements of an array to a single value 
+*/ 
+
+const prices = [5, 30, 10, 25, 15, 30];
+const total = prices.reduce(sum);   // has a single value 
+
+console.log(`$${total.toFixed(2)}`);
+
+function sum(previous, next) {    // function sum(accumulator, element)
+  return previous + next; 
+}
+
+
+const grades = [75, 50, 2, 90, 80, 10, 65, 95];
+
+const maxValue = grades.reduce(getMax);    // pass callback, find the max value
+const minValue = grades.reduce(getMin);   // find the min value
+
+console.log(maxValue);
+console.log(minValue);
+
+// return the max value in the array
+function getMax(accumulator, element) {
+  return Math.max(accumulator, element);
+}
+
+// return the min value in the array
+function getMin(accumulator, element) {
+  return Math.min(accumulator, element);
+}
+
+// find the shortest word
+const words = ['one', 'three', 'a', 'as', 'nine'];
+const shortest = words.reduce(getShortestWord);
+console.log(shortest);
+
+function getShortestWord(accumulator, word) {
+  return word.length < accumulator.length ? word : accumulator;
+}
+
+// find the longest word
+const longest = words.reduce(getLongestWord);
+console.log(longest);
+
+function getLongestWord(accumulator, word) {
+  return word.length > accumulator.length ? word : accumulator;
+}
