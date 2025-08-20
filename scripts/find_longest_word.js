4352453f5eaaaeb71ab91072de57af1c263ca3e8
Mira Desaville <miradeldesaville@gmail.com>
2025-08-20 13:17:26 +0800

    Updated findLongestWord function and added findShortestWord function

--- scripts/find_longest_word.js
+++ scripts/find_longest_word.js
@@ -1,17 +1,37 @@
 
-const array = ['onedotoneefoydfghj','apple', 'banana', 'kiwi', 'strawberry', 'onedotoneefoy'];
+const array = ['w','oned','apple', 'banana', 'iwi', 'onedotoneefoydfghj', 'strawberry', 'onedotoneefoy', 'ww'];
 
 console.log(findLongestWord(array));
+console.log(findShortestWord(array));
 
 function findLongestWord(array) {
-  let longestWord = []; 
+  let longestWord = ''; // not an array bu an empty str (empty str has 0 length)
 
   for (let i = 0; i < array.length; i++) {
     let word = array[i];
 
-    if (word.length >= array[0].length) {
+    if (word.length > longestWord.length) {
       longestWord = word;
+        // changes made:
+        /* cuurent element should not only compared to the first element [0]
+          but to each element
+           */
     }
   }
   return longestWord; 
+}
+
+
+function findShortestWord(array) {
+  // need to initialize the first word
+  let shortestWord = array[0];  // 
+
+  for (let i = 0; i < array.length; i++) {
+    let word = array[i];
+
+    if (word.length < shortestWord.length) {
+      shortestWord = word;
+    }
+  }
+  return shortestWord;
 }
